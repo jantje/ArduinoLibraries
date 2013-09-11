86c2df00ad3db6f7a810e425ab65900b9f3efbe1
jantje <eclipse@baeyens.it>
2013-09-11 20:20:20 +0200

    Added BlinkLed a C++ blink without delay class
    This is build upon blink without delay example.
    I  added a different delay time for on as off

---
+++ BlinkLed/BlinkLed.cpp
@@ -0,0 +1,65 @@
+/*
+ * BlinkLed.cpp
+ *
+ *  Created on: Sep 11, 2013
+ *      Author: jan
+ */
+
+#include "BlinkLed.h"
+
+BlinkLed::BlinkLed(uint8_t ledPin,uint32_t onInterval ,uint32_t offInterval )
+{
+
+	myLedPin =  ledPin;      // the number of the LED pin
+	myLedState = LOW;             // ledState used to set the LED
+	myPreviousMillis = millis();        // will store last time LED was updated
+	myOnInterval = onInterval;
+	myOffInterval=offInterval;
+}
+
+
+
+
+void BlinkLed::setup() {
+  // set the digital pin as output:
+  pinMode(myLedPin, OUTPUT);
+  digitalWrite(myLedPin, myLedState);
+}
+
+void BlinkLed::loop()
+{
+  // here is where you'd put code that needs to be running all the time.
+
+  // check to see if it's time to blink the LED; that is, if the
+  // difference between the current time and last time you blinked
+  // the LED is bigger than the interval at which you want to
+  // blink the LED.
+	uint32_t myInterval =myOffInterval;
+  if (myLedState == LOW)
+  {
+  	myInterval =myOnInterval;
+  }
+
+
+  unsigned long currentMillis = millis();
+
+  if(currentMillis - myPreviousMillis > myInterval) {
+    // save the last time you blinked the LED
+  	myPreviousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (myLedState == LOW)
+    {
+    	myLedState = HIGH;
+    	myInterval =myOnInterval;
+    }
+    else
+    {
+    	myLedState = LOW;
+    	myInterval =myOffInterval;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(myLedPin, myLedState);
+  }
+}
---
+++ BlinkLed/BlinkLed.h
@@ -0,0 +1,35 @@
+/*
+ * BlinkLed.h
+ *
+ *  Created on: Sep 11, 2013
+ *      Author: jan
+ *      This is the blink without delay example put in a class
+ *      I made the on interval different from the off interval
+ */
+
+#ifndef BLINKLED_H_
+#define BLINKLED_H_
+#include "Arduino.h"
+
+class BlinkLed
+{
+	protected:
+		// constants won't change. Used here to
+		// set pin numbers:
+		uint8_t myLedPin ;      // the number of the LED pin
+
+		// Variables will change:
+		uint8_t myLedState ;             // ledState used to set the LED
+		uint32_t myPreviousMillis ;        // will store last time LED was updated
+
+		// the follow variables is a long because the time, measured in miliseconds,
+		// will quickly become a bigger number than can be stored in an int.
+		uint32_t myOnInterval ;           // interval at which to blink (milliseconds)
+		uint32_t myOffInterval ;           // interval at which to blink (milliseconds)
+	public:
+		BlinkLed(uint8_t ledPin,uint32_t onInterval ,uint32_t offInterval);
+		void setup();
+		void loop();
+};
+
+#endif /* BLINKLED_H_ */
