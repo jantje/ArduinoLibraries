ece18fce5002749ce0d1cd9d6559f915bfe601ca
jantje <jan@baeyens.it>
2016-02-01 16:31:52 +0100

    support for 32 it systems
    basically int to int16_t

--- simplot/simplot.h
+++ simplot/simplot.h
@@ -8,20 +8,20 @@ A library to provide basic support for the simplot oscilloscope
 
 
 /* use the general plot method or one of the wrapping macros*/
-void _simple_plot_(Stream &output, byte numData, int data1, int data2, int data3, int data4, int data5, int data6);
+void _simple_plot_(Stream &output, uint8_t numData, int16_t data1, int16_t data2, int16_t data3, int16_t data4, int16_t data5, int16_t data6);
 
 
 
 #define plot6(output,  data1,  data2,  data3,  data4,  data5,  data6) _simple_plot_(output, 6, data1, data2, data3,  data4,  data5,  data6)
 
-#define plot5(output,  data1,  data2,  data3,  data4,  data5 ) _simple_plot_(output, 6, data1, data2, data3,  data4,  data5,  (millis() % 1000) / 4)
+#define plot5(output,  data1,  data2,  data3,  data4,  data5 ) _simple_plot_(output, 5, data1, data2, data3,  data4,  data5,  0)
 
-#define plot4(output,  data1,  data2,  data3,  data4) _simple_plot_(output, 5, data1, data2, data3,  data4,  (millis() % 1000) / 4,  0)
+#define plot4(output,  data1,  data2,  data3,  data4) _simple_plot_(output, 4, data1, data2, data3,  data4,  0,  0)
 
-#define plot3(output,  data1,  data2,  data3) _simple_plot_(output, 4, data1, data2, data3,  (millis() % 1000) / 4,  0,  0)
+#define plot3(output,  data1,  data2,  data3) _simple_plot_(output, 3, data1, data2, data3,  0,  0,  0)
 
-#define plot2(output,  data1,  data2) _simple_plot_(output, 3, data1, data2, (millis() % 1000) / 4,  0,  0,  0)
+#define plot2(output,  data1,  data2) _simple_plot_(output, 2, data1, data2, 0,  0,  0,  0)
 
-#define plot1(output,  data1) _simple_plot_(output, 2, data1, (millis() % 1000) / 4, 0,  0,  0,  0)
+#define plot1(output,  data1) _simple_plot_(output, 1, data1, 0, 0,  0,  0,  0)
 
 #endif //simplot_H_
