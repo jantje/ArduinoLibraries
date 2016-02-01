c82a60c718515dc669f495f0a899e7e35bb7e74a
jantje <jan@baeyens.it>
2016-02-01 16:32:43 +0100

    support for 32 bit systems

--- simplot/simplot.cpp
+++ simplot/simplot.cpp
@@ -1,23 +1,31 @@
 #include "simplot.h"
 
+void _simple_plot_(Stream &output, byte numData, int16_t data1, int16_t data2,
+		int16_t data3, int16_t data4, int16_t data5, int16_t data6) {
+	uint8_t plotBuffer[16];
+	uint16_t pktSize;
+	uint16_t numDataBytes = numData * sizeof(uint16_t); //Size of data in bytes. Does not include the header and size fields
 
-void _simple_plot_(Stream &output, byte numData, int data1, int data2, int data3, int data4, int data5, int data6)
-{
-	int plotBuffer[8];
-  int pktSize;
+	plotBuffer[0] = 0xAB;
+	plotBuffer[1] = 0xCD; //SimPlot packet header. Indicates start of data packet
+	plotBuffer[2] = lowByte(numDataBytes);
+	plotBuffer[3] = highByte(numDataBytes);
+	plotBuffer[4] = lowByte(data1);
+	plotBuffer[5] = highByte(data1);
+	plotBuffer[6] = lowByte(data2);
+	plotBuffer[7] = highByte(data2);
+	plotBuffer[8] = lowByte(data3);
+	plotBuffer[9] = highByte(data3);
+	plotBuffer[10] = lowByte(data4);
+	plotBuffer[11] = highByte(data4);
+	plotBuffer[12] = lowByte(data5);
+	plotBuffer[13] = highByte(data5);
+	plotBuffer[14] = lowByte(data6);
+	plotBuffer[15] = highByte(data6);
 
-  plotBuffer[0] = 0xCDAB;             //SimPlot packet header. Indicates start of data packet
-  plotBuffer[1] = numData*sizeof(int);      //Size of data in bytes. Does not include the header and size fields
-  plotBuffer[2] = data1;
-  plotBuffer[3] = data2;
-  plotBuffer[4] = data3;
-  plotBuffer[5] = data4;
-  plotBuffer[6] = data5;
-  plotBuffer[7] = data6;
+	pktSize = (numData + 2) * sizeof(uint16_t); //Header bytes + size field bytes + data
 
-  pktSize =  (numData+2)*sizeof(int); //Header bytes + size field bytes + data
-
-  output.write((uint8_t * )plotBuffer, pktSize);
- // output.println();
+	output.write((uint8_t *) plotBuffer, pktSize);
+	//output.println();
 }
 
