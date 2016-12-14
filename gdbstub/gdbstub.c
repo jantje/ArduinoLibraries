31915c91609211c54bfdbf4fe3b7a7742116efb2
jantje <eclipse@baeyens.it>
2016-12-14 01:07:56 +0100

    trying to get esp debugging going

---
+++ gdbstub/gdbstub.c
@@ -0,0 +1,6 @@
+
+#include "gdbstub.h"
+
+
+
+
---
+++ gdbstub/gdbstub.h
@@ -0,0 +1,19 @@
+#ifndef GDBSTUB_H
+#define GDBSTUB_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void gdbstub_init()
+{
+}
+;
+
+#ifdef __cplusplus
+}
+#endif
+
+#define RAMFUNC
+
+#endif
