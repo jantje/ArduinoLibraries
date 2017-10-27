0269f079957883c6269b377ee1d0f5506959e273
jantje <eclipse@baeyens.it>
2017-10-27 23:38:51 +0200

    fixed some typo's

--- BlinkLedSerial/BlinkLedSerial.h
+++ BlinkLedSerial/BlinkLedSerial.h
@@ -1,7 +1,7 @@
 /*
  * BlinkLedSerial.h
  *
- * This is the extension neededto get blinkled to be controlled by the serial comunication module
+ * This is the extension needed to get blinkled to be controlled by the serial communication module
  *
  *  Created on: Nov 7, 2014
  *      Author: jan
@@ -13,12 +13,13 @@
 #include <BlinkLed.h>
 #include "SerialDataInterface.h"
 
-class BlinkLedSerial: public BlinkLed
-{
-	public:
-		BlinkLedSerial(uint8_t ledPin,uint32_t onInterval ,uint32_t offInterval):
-			BlinkLed( ledPin, onInterval , offInterval){};
-		void serialRegister(const __FlashStringHelper* Name);
+class BlinkLedSerial: public BlinkLed {
+public:
+	BlinkLedSerial(uint8_t ledPin, uint32_t onInterval, uint32_t offInterval) :
+			BlinkLed(ledPin, onInterval, offInterval) {
+	}
+	;
+	void serialRegister(const __FlashStringHelper* Name);
 };
 
 #endif /* LIBRARIES_BLINKLED_BLINKLEDSERIAL_H_ */
