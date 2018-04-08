e2cce83e2b7000ccc179bc5edc03ab05b6b775af
jantje <eclipse@baeyens.it>
2018-04-08 23:48:18 +0200

    reimplemented USE_SERIAl_LOGGING

--- VoltMeter/VoltMeter.cpp
+++ VoltMeter/VoltMeter.cpp
@@ -7,60 +7,77 @@
 
 #include "VoltMeter.h"
 
-VoltMeter::VoltMeter(uint8_t Pin)
+#ifdef USE_SERIAl_LOGGING
+void VoltMeter::serialRegister(const __FlashStringHelper* Name)
 {
-	myPin=Pin;
-	myCentiVolt=0;
-	myMultiplyerValue=1000;
-	myActualReadValue=0;
-	myAveragedReadValue=0;
-}
+    FieldData::set(Name,F("CentiVolt"),0,&myCentiVolt);
+    FieldData::setNext((__FlashStringHelper *)MULTIPLIER_DIV_1000,MOD_WRITE|MOD_SAVE,&myMultiplyerValue);
 
+#ifdef DETAILED_SERIAL_LOGGING
+    FieldData::setNext(F("Value"),0,&myActualReadValue);
+    FieldData::setNext(F("AvgValue"),0,&myAveragedReadValue);
+    FieldData::setNext((__FlashStringHelper *)PIN,0,&myPin);
+    //          FieldData::setNext( (__FlashStringHelper *)LOOPDURATION,0,&myLoopduration);
+#endif
+
+}
+VoltMeter::VoltMeter(uint8_t Pin) {
+    myPin = Pin;
+    myCentiVolt = 0;
+    myMultiplyerValue = 1000;
+    myActualReadValue = 0;
+    myAveragedReadValue = 0;
+}
+#else
+VoltMeter::VoltMeter(uint8_t Pin, uint16_t multiplyer) {
+    myPin = Pin;
+    myCentiVolt = 0;
+    myMultiplyerValue = multiplyer;
+    myActualReadValue = 0;
+    myAveragedReadValue = 0;
+}
+#endif
 
 #ifdef PRECISE_VOLTAGE
 long readVcc() {
-  long result;
-  // Read 1.1V reference against AVcc
-  //ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
-  ADCSRB &= ~_BV(MUX5) ;
-  delay(2); // Wait for Vref to settle
-  ADCSRA |= _BV(ADSC); // Convert
-  while (bit_is_set(ADCSRA,ADSC));
-  result = ADCL;
-  result |= ADCH<<8;
-  result = 1125300L / result; // Back-calculate AVcc in mV
-  return result;
+    long result;
+    // Read 1.1V reference against AVcc
+    //ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+    ADCSRB &= ~_BV(MUX5);
+    delay(2);// Wait for Vref to settle
+    ADCSRA |= _BV(ADSC);// Convert
+    while (bit_is_set(ADCSRA,ADSC));
+    result = ADCL;
+    result |= ADCH<<8;
+    result = 1125300L / result;// Back-calculate AVcc in mV
+    return result;
 }
 #endif //PRECISE_VOLTAGE
 
-void VoltMeter::loop()
-{
-#ifdef PRECISE_VOLTAGE
-	long vcc=readVcc();
-	myActualReadValue= (analogRead(myPin) *vcc) / 1023;
-	//as we do precise reading there is no need to average
-	myAveragedReadValue=myActualReadValue;
+void VoltMeter::loop() {
+    //only reed every 100 ms
+#ifdef LOOPMILLIS
+    extern uint32_t LOOPMILLIS;
 #else
-	myActualReadValue=  analogRead(myPin);
-	//As we do not read precisely we average the result
-	myAveragedReadValue=(myAveragedReadValue*8+(2*myActualReadValue))/10;
+#define LOOPMILLIS curMillis
+    uint32_t LOOPMILLIS = millis();
+
+#endif
+    if (LOOPMILLIS - my_last_read > 100) {
+        my_last_read = LOOPMILLIS;
+#ifdef PRECISE_VOLTAGE
+        long vcc=readVcc();
+        myActualReadValue= (analogRead(myPin) *vcc) / 1023;
+        //as we do precise reading there is no need to average
+        myAveragedReadValue=myActualReadValue;
+#else
+        myActualReadValue = analogRead(myPin);
+        //As we do not read precisely we average the result
+        myAveragedReadValue = (myAveragedReadValue * 8 + (2 * myActualReadValue)) / 10;
 #endif
 
-	myCentiVolt=(uint16_t)(((uint32_t)myAveragedReadValue*(uint32_t)myMultiplyerValue)/1000UL);
+        myCentiVolt = (uint16_t)(((uint32_t) myAveragedReadValue * (uint32_t) myMultiplyerValue) / 1000UL);
+    }
 }
 
-
-		void VoltMeter::serialRegister(const __FlashStringHelper* Name)
-		{
-			FieldData::set(Name,F("CentiVolt"),0,&myCentiVolt);
-			FieldData::setNext((__FlashStringHelper *)MULTIPLIER_DIV_1000,MOD_WRITE|MOD_SAVE,&myMultiplyerValue);
-
-#ifdef DETAILED_SERIAL_LOGGING
-			FieldData::setNext(F("Value"),0,&myActualReadValue);
-			FieldData::setNext(F("AvgValue"),0,&myAveragedReadValue);
-			FieldData::setNext((__FlashStringHelper *)PIN,0,&myPin);
-			//			FieldData::setNext( (__FlashStringHelper *)LOOPDURATION,0,&myLoopduration);
-#endif
-
-		}
--- VoltMeter/VoltMeter.h
+++ VoltMeter/VoltMeter.h
@@ -5,12 +5,13 @@
  *      Author: jan
  */
 
-#ifndef VOLTMETER_H_
-#define VOLTMETER_H_
+#pragma once
 
 
 #include "Arduino.h"
+#ifdef USE_SERIAl_LOGGING
 #include "SerialDataInterface.h"
+#endif
 
 class VoltMeter
 {
@@ -20,23 +21,38 @@ class VoltMeter
 		uint16_t myMultiplyerValue;
 		uint16_t myActualReadValue;
 		uint16_t myAveragedReadValue;
-		//uint32_t myLoopduration;
+		uint32_t my_last_read;
+
 
 	public:
+#ifdef USE_SERIAl_LOGGING
+		void serialRegister(const __FlashStringHelper* Name);
 		VoltMeter( uint8_t Pin);
+#else
+		/*
+		 * Constructor class if you do not use the serial logger.
+		 * This class allows you to initialize some constants that are otherwise
+		 * set via serial communication and remembered in FI eeprom
+		 * multiplyer is used to get from the adc cnverter (1 to 1023) to centivolt as follows
+		 * centivolt=adcvalue*multiplyer/1000
+		 * Assume you have a 10volt source and you uses a 50/50 deviding bridge on a uno
+		 * this means that 10 volt on source is 5 volt on the pin or 1023 adcvalue
+		 * the formula is the
+		 * 1000(10Volt=1000centivolt)=1023*multiplyer/1000
+		 * this mean multiplyer=1000*1000/1023=978
+		 */
+		VoltMeter(uint8_t Pin,uint16_t multiplyer);
+#endif
 
 		void setup()
 		{
-			//pinMode(myPin,INPUT); This is not needed
-			loop(); //initialize the average
-			myAveragedReadValue=myActualReadValue;
+			pinMode(myPin,INPUT); // This may not be needed
+			myAveragedReadValue=myActualReadValue= analogRead(myPin);
 		}
 
 		void loop();
 		uint16_t getCentiVoltage() const	{	return myCentiVolt;}
 		uint16_t get_dV() const	{	return (myCentiVolt+5)/10;}
 
-		void serialRegister(const __FlashStringHelper* Name);
 };
 
-#endif /* VOLTMETER_H_ */
