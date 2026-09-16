c5babc31d5983329318da0df6a477b5d948c1ff0
jan <jan@192.168.1.194>
2026-09-16 04:04:37 +0200

    Some cleanup because of amp meter in siemens

--- CurrentSensorInterface/CurrentSensorInterface.h
+++ CurrentSensorInterface/CurrentSensorInterface.h
@@ -1,21 +1,20 @@
 /*
- * ICurrentSensor.h
+ * CurrentSensorInterface.h
  *
  *  Created on: Jul 14, 2014
  *      Author: jan
  */
 
-#ifndef ICURRENTSENSOR_H_
-#define ICURRENTSENSOR_H_
+#pragma once
 #include "Arduino.h"
 
 class CurrentSensorInterface
 {
 	protected:
-		uint16_t myCentiAmp;  //current in centi amps
-		uint16_t myAvgCentiAmp; //average current in centi amps
+		uint16_t myCentiAmp=0;  //current in centi amps
+		uint16_t myAvgCentiAmp=0; //average current in centi amps
 	public:
-		CurrentSensorInterface(){		myCentiAmp=0;		myAvgCentiAmp=0;}
+		CurrentSensorInterface(){}
 		virtual void loop()=0;
 		virtual void setup()=0;
 		uint16_t getCurrent_cA() {return myCentiAmp;}
@@ -24,7 +23,4 @@ class CurrentSensorInterface
 		uint16_t getAvgCurrent_cA() {return myAvgCentiAmp;}
 		uint16_t getAvgCurrent_dA() {return (myAvgCentiAmp+5)/10;}
 		uint16_t getAvgCurrent_A() {return (myAvgCentiAmp+50)/100;}
-		virtual void setZeroAmp()=0;
 };
-
-#endif /* ICURRENTSENSOR_H_ */
